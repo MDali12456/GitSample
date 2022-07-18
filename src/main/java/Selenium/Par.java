4f9228abb32f56d44153dcc0e9c91bf88093c99f
MDali12456 <mohammedali99335@gmail.com>
2022-07-18 16:23:27 +0530

    first git commit

---
+++ src/main/java/Selenium/Par.java
@@ -0,0 +1,15 @@
+package Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class Par {
+@Test
+public void Par() {
+	System.out.println("the");
+}
+	
+
+}
